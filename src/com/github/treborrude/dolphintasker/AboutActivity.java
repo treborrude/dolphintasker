6ddba534ac2f0868afa76761f314ac2c631c1b24
Trebor Rude <treborrude@googlemail.com>
2014-05-24 09:09:15 -0600

    Dolphin 'About' activity.

---
+++ src/com/github/treborrude/dolphintasker/AboutActivity.java
@@ -0,0 +1,9 @@
+package com.github.treborrude.dolphintasker;
+
+public class AboutActivity extends com.dolphin.browser.addons.AboutActivity
+{
+    @Override
+    protected void onStartCustomAboutActivity() {
+	  
+	}
+}
