ffcf93aa23f53e6640f35b4b06da5784e7cb59df
Trebor Rude <treborrude@googlemail.com>
2014-05-24 10:16:39 -0600

    Moved to ui package.

--- src/com/github/treborrude/dolphintasker/AboutActivity.java
+++
@@ -1,9 +0,0 @@
-package com.github.treborrude.dolphintasker;
-
-public class AboutActivity extends com.dolphin.browser.addons.AboutActivity
-{
-    @Override
-    protected void onStartCustomAboutActivity() {
-	  
-	}
-}
---
+++ src/com/github/treborrude/dolphintasker/ui/AboutActivity.java
@@ -0,0 +1,11 @@
+package com.github.treborrude.dolphintasker.ui;
+
+import com.dolphin.browser.addons.AboutActivity;
+
+public class AboutActivity extends AboutActivity
+{
+    @Override
+    protected void onStartCustomAboutActivity() {
+	  super.onStartCustomAboutActivity();
+	}
+}
