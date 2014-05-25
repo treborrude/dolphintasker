dae1d26d55f87b8d7c34d886561929becb1ff045
Trebor Rude <treborrude@googlemail.com>
2014-05-25 16:58:38 -0600

    App-wide constants.

---
+++ src/com/github/treborrude/dolphintasker/Constants.java
@@ -0,0 +1,11 @@
+package com.github.treborrude.dolphintasker;
+
+public class Constants
+{
+  public static String CONDITION = "com.github.treborrude.dolphintasker.condition";
+  public static String EVENT_DETECTED = "com.github.treborrude.dolphintasker.event_detected";
+  
+  private Constants()
+  {
+  }
+}
