c5bf06653c4cfc334bd3fe793b98b268099c6623
Trebor Rude <treborrude@googlemail.com>
2014-05-25 16:59:13 -0600

    New edit activity for receive title event.

---
+++ src/com/github/treborrude/dolphintasker/ui/ReceiveTitleEditActivity.java
@@ -0,0 +1,28 @@
+package com.github.treborrude.dolphintasker.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import com.github.treborrude.dolphintasker.R;
+
+public class ReceiveTitleEditActivity extends Activity
+{
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+	// TODO: Implement this method
+	super.onCreate(savedInstanceState);
+
+	final Intent resultIntent = new Intent();
+	final Bundle resultBundle = new Bundle();
+
+	resultBundle.putString(com.github.treborrude.dolphintasker.Constants.CONDITION,
+	                       getResources().getString(R.string.receive_title));
+	resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE, resultBundle);
+	resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_STRING_BLURB, "");
+
+	setResult(RESULT_OK, resultIntent);
+	setResult(Activity.RESULT_OK);
+	finish();
+  }
+}
