e0ee4ccc7a8dd87bb65ebf3256c65189277cd7f8
Trebor Rude <treborrude@googlemail.com>
2014-05-25 17:10:55 -0600

    New activity for page started event.

---
+++ src/com/github/treborrude/dolphintasker/ui/PageStartedEditActivity.java
@@ -0,0 +1,30 @@
+package com.github.treborrude.dolphintasker.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import com.github.treborrude.dolphintasker.R;
+
+public class PageStartedEditActivity extends Activity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+	// TODO: Implement this method
+	super.onCreate(savedInstanceState);
+
+	final Intent resultIntent = new Intent();
+	final Bundle resultBundle = new Bundle();
+	
+	resultBundle.putString(com.github.treborrude.dolphintasker.Constants.CONDITION,
+	                       getResources().getString(R.string.page_started));
+	resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE, resultBundle);
+	resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_STRING_BLURB, "");
+
+	setResult(RESULT_OK, resultIntent);
+	setResult(Activity.RESULT_OK);
+	finish();
+  }
+  
+}
