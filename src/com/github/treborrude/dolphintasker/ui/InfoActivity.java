ce2b3d090e3b335e82f0cbd8020a67c440979e6c
Trebor Rude <treborrude@googlemail.com>
2014-06-04 13:59:34 -0600

    Create activity for About in Dolphin and information in the Play store.

---
+++ src/com/github/treborrude/dolphintasker/ui/InfoActivity.java
@@ -0,0 +1,16 @@
+package com.github.treborrude.dolphintasker.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.github.treborrude.dolphintasker.R;
+
+public class InfoActivity extends Activity
+{
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.info);
+  }
+  
+}
