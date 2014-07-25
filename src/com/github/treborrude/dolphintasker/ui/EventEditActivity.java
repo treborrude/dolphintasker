2d4ec6b9cf3bef9bf3fcae40083a7ebeda451bbe
Trebor Rude <treborrude@googlemail.com>
2014-07-25 08:21:41 -0600

    Modifications to support one-activity-multiple-fragments design.

--- src/com/github/treborrude/dolphintasker/ui/EventEditActivity.java
+++ src/com/github/treborrude/dolphintasker/ui/EventEditActivity.java
@@ -1,16 +1,14 @@
 package com.github.treborrude.dolphintasker.ui;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.content.Intent;
-import com.github.treborrude.dolphintasker.R;
-import android.view.View;
-import android.widget.RadioGroup;
-import com.github.treborrude.dolphintasker.TaskerPlugin;
-import android.view.Menu;
-import android.view.MenuItem;
-import com.github.treborrude.dolphintasker.ui.EventSelectFragment;
 import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import com.github.treborrude.dolphintasker.R;
+import com.github.treborrude.dolphintasker.TaskerPlugin;
+import com.github.treborrude.dolphintasker.ui.EventSelectFragment;
+import android.app.FragmentTransaction;
+import android.util.Log;
 
 // Activity which is called by Tasker to configure DolphinTasker events.
 // Tasker expects this activity to return a result. Due to that, and
@@ -22,44 +20,81 @@ public class EventEditActivity
   extends Activity 
   implements EventSelectFragment.EventTypeSelected
 {
-
+  private static final String LOG_TAG = "EventEditActivity";
+  
+  private ConfigurationFragment mConfigurationFragment = null;
+  
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
 	super.onCreate(savedInstanceState);
+    setContentView(R.layout.event_edit_activity);
 
-    FragmentManager fragmentManager = getFragmentManager();
-
-    fragmentManager.beginTransaction().add(new EventSelectFragment(), "event_select").commit();	
-	final Bundle localeBundle = getIntent().getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
-	if (localeBundle == null)
+	// Prevent overlapping fragments.
+	if (savedInstanceState != null)
 	{
+	  // As near as I can tell, if savedInstanceState is not null,
+	  // Android is restoring this activity from a previous state,
+	  // and it is not necessary to check the Intent that launced
+	  // this Activity for Locale information.
+
 	  return;
 	}
+	
+	if (findViewById(R.id.fragment_container) != null)
+	{	  
+      getFragmentManager().beginTransaction()
+	      .add(R.id.fragment_container, new EventSelectFragment(), "event_select").commit();
+	}
+	
+	final Bundle localeBundle = getIntent().getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
+	// TODO: Use localeBundle to restore configuration fragment and selected
+	// item state.
   }
 
   @Override
-  public void onEventTypeSelected(String event_type)
+  public void onEventTypeSelected(String event_type, String event_name)
   {
-	// TODO: Launch details activity for selected event.
-	String[] event_types = getResources().getStringArray(R.array.tasker_events);
-  }
-  
-  public void endConfiguration(View view)
-  {
-	String event = null;
-	String[] relevantVariables = null;
+	mConfigurationFragment = new ConfigurationFragment(event_type, event_name);
 	
-	if (event != null)
+	int fragmentContainerId = 0;
+	
+	if (findViewById(R.id.fragment_container) != null)
+	{
+	  fragmentContainerId = R.id.fragment_container;
+	}
+	else if (findViewById(R.id.configuration_fragment) != null)
+	{
+	  fragmentContainerId = R.id.configuration_fragment;
+	}
+	else
+	{
+	  Log.e(LOG_TAG, "Unable to find container for configuration fragment!");
+	  // TODO: Dialog box telling the user that things are badly broken.
+	  finish();
+	}
+	
+	FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+	fragmentTransaction.replace(fragmentContainerId, mConfigurationFragment, "configuration_fragment");
+	fragmentTransaction.addToBackStack(null);
+	fragmentTransaction.commit();
+  }
+
+  @Override
+  public void finish()
+  {
+	Bundle savedConfiguration = 
+	    (mConfigurationFragment != null) ? mConfigurationFragment.getSavedConfiguration() : null;
+	
+	if (savedConfiguration != null)
 	{
 	  final Intent resultIntent = new Intent();
-	  final Bundle resultBundle = new Bundle();
 	  
-	  resultBundle.putString(com.github.treborrude.dolphintasker.Constants.CONDITION,
-							 event);
-	  resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE, resultBundle);
-	  resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_STRING_BLURB, event);
+	  resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE, savedConfiguration);
+	  resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_STRING_BLURB, mConfigurationFragment.getBlurb());
 
+	  String[] relevantVariables = mConfigurationFragment.getRelevantVariables();
+	  
 	  if (TaskerPlugin.hostSupportsRelevantVariables(getIntent().getExtras()) &&
 	      relevantVariables != null)
 	  {
@@ -68,8 +103,6 @@ public class EventEditActivity
 	  setResult(RESULT_OK, resultIntent);
 	}
 	
-	finish();
-  }
-
-  
+	super.finish();
+  }  
 }
