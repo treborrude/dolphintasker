07bf6d85422e1687fd8ea6b951a57662a7725dcc
Trebor Rude <treborrude@googlemail.com>
2014-08-01 11:13:17 -0600

    Change constants as needed for the new design.

--- src/com/github/treborrude/dolphintasker/Constants.java
+++ src/com/github/treborrude/dolphintasker/Constants.java
@@ -2,11 +2,11 @@ package com.github.treborrude.dolphintasker;
 
 public class Constants
 {
-  public static String CONDITION = "com.github.treborrude.dolphintasker.condition";
-  public static String PREFS_NAME = "ReturnValues";
-  public static String PF_KEY = "PageFinished";
-  public static String PS_KEY = "PageStarted";
-  public static String RT_KEY = "ReceivedTitle";
+  private final static String PREFIX = Constants.class.getPackage().getName();
+  
+  public final static String EVENT_DATA = PREFIX + ".EVENT_DATA";
+  public final static String EVENT_DETECTED = PREFIX + ".EVENT_DETECTED";
+  public final static String EVENT_TYPE = PREFIX + ".EVENT_TYPE";
   
   private Constants()
   {
